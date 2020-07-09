ef1215386cb320ee59221ce29e6f3f7cdfcd3914
nas <nas.silver.bullet@gmail.com>
2020-07-10 01:24:36 +0900

    :+1: Update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 江添亮のC++入門
 
 Refs: <https://ezoeryou.github.io/cpp-intro/>
+
+続き: <https://ezoeryou.github.io/cpp-intro/#vector>
--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
 #include "all.h"
 
-int main() { std::cout << "hello"; }
+int input() {
+  int x{};
+  std::cin >> x;
+  return x;
+}
+
+int factorial(int n) {
+  if (n < 2) {
+    return n;
+  }
+  return n * factorial(n - 1);
+}
+
+int main() {
+  auto n = input();
+  std::cout << factorial(n) << std::endl;
+}
