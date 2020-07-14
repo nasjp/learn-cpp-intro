97bd95338fcd209b7c2312540135488c0ce189e1
nas <nas.silver.bullet@gmail.com>
2020-07-14 19:59:52 +0900

    :+1: Update

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Refs: <https://ezoeryou.github.io/cpp-intro/>
 
-続き: <https://ezoeryou.github.io/cpp-intro/#for_each>
+続き: <https://ezoeryou.github.io/cpp-intro/#%E3%82%AF%E3%83%A9%E3%82%B9%E3%81%AE%E5%9F%BA%E6%9C%AC>
--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
 #include "all.h"
 
-auto print = [](auto const &x) { std::cout << x << std::endl; };
-
 int main() {
-  int x = 0;
-
-  int &ref = x;
-  // OK
-  ++ref;
-
-  const int &const_ref = ref;
-
-  print(const_ref);
-
-  ++ref;
-  print(const_ref);
+  [](bool b) -> int {
+    if (b)
+      return 0;
+    else
+      return 0.0;
+  }(true);
 }
